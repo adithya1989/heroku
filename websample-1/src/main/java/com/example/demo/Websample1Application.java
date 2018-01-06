5985b654078697847f9fcfa8b7b60b1138c46f81
adithya421 <adithya.inugurthi@gmail.com>
2018-01-06 21:46:06 +0300

    spring update 2

--- websample-1/src/main/java/com/example/demo/Websample1Application.java
+++ websample-1/src/main/java/com/example/demo/Websample1Application.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,5 +18,10 @@ public class Websample1Application {
 	 @GetMapping("/")
 	  public String hello() {
 	    return "hello world!";
+	  }
+	 
+	 @GetMapping("/name/{name}")
+	  public String name(@PathVariable(value="name")String name) {
+	    return "hello world! "+name;
 	  }
 }
